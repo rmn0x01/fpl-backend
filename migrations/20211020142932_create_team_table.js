b0c4e20b63e93a3d895630cc3270ac3cd471df4f
rmn0x01 <mnizar9292@gmail.com>
2021-10-24 20:40:11 +0700

    db: update team+player table, add knex cmd

--- migrations/20211020142932_create_team_table.js
+++ migrations/20211020142932_create_team_table.js
@@ -3,6 +3,7 @@ exports.up = async function(knex) {
     await knex.raw(`CREATE TABLE fpl.teams (
         team_id varchar(50) NOT NULL,
         team_nm varchar(50) NOT NULL,
+        team_img text NULL,
         is_on_pl bool NOT NULL DEFAULT true,
         CONSTRAINT teams_pkey PRIMARY KEY (team_id)
     )`)
--- migrations/20211020143516_create_player_table.js
+++ migrations/20211020143516_create_player_table.js
@@ -3,6 +3,7 @@ exports.up = async function(knex) {
     await knex.raw(`CREATE TABLE fpl.players (
         player_id varchar(50) NOT NULL,
         player_nm varchar(50) NOT NULL,
+        player_img text NULL,
         team_id varchar(50) NOT NULL,
         CONSTRAINT players_pkey PRIMARY KEY (player_id)
     )`)
